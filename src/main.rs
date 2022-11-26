11465ee91f1af59c0bea7f341d38ed064a8d5af3
Edward Naidoo <securedbread@protonmail.com>
2022-11-25 17:35:36 -0800

    Added Code

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+fn main() {
+    loop {
+        let mut guess = String::new();
+        
+        // Generated Variable Value
+        let secret_number = rand::thread_rng().gen_range(1..101);
+        println!("The secret number is: {}", secret_number);
+    
+        println!("Guess the number!");
+        println!("Please input your guess.");
+    
+        // Collects user input and puts it in "guess"
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("{}", "Too Big".red()),
+            Ordering::Equal => {
+                println!("{}", "Correct!".green().bold());
+                break;
+            },
+            Ordering::Less => println!("{}", "Too Small".yellow())
+        }
+    }
+}
